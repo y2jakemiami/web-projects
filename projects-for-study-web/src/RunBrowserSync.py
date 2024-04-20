9f944905792b43f43f9a518772798d96185aa13f
y2jakemiami <167628439+y2jakemiami@users.noreply.github.com>
2024-04-20 16:52:08 +0300

    Add files via upload

---
+++ projects-for-study-web/src/RunBrowserSync.py
@@ -0,0 +1,5 @@
+import os
+import pyautogui
+
+os.system('cmd /k cd C:/Users/Камиль/documents/kamil/projects-for-study-web/src&browser-sync start --server --files "*.html"') 
+os.system('browser-sync start --server --files "*.html"') 
